3dca3daccda4fd4052b296f5cc245a5cbb3b8dca
June <adasilva.html@gmail.com>
2022-09-24 21:14:29 -0400

    Create calculator console & inner section outlines

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // ADD function
 const add = (a, b) => Number(a) + Number(b);
-console.log(`Add 2 + 2: ${add(2, 2)}`);
+
 
 // SUBTRACT function
 const subtract = (a, b) => Number(a) - Number(b);
-console.log(`Subtract 3 - 5: ${subtract(3, 5)}`);
+
 
 // DIVIDE function
 const divide = (a, b) => Math.round(Number(a) / Number(b) * 100 ) / 100;
-console.log(`Divide 2 / 3: ${divide(2, 3)}`);
+
 
 // MULTIPLY function
 const multiply = (a, b) => Number(a) * Number(b);
-console.log(`Multiply 5 * 2: ${multiply(5, 2)}`);
 
+// operate function takes in input and decides which operation to activate
 const operate = (numOne, operator, numTwo) => {
     if (operator === '+') {
         return add(numOne, numTwo);
@@ -28,10 +28,4 @@ const operate = (numOne, operator, numTwo) => {
         return multiply(numOne, numTwo);
     }
 }
-
-console.log(`Operator = Add 2 + 2: ${operate(2, "+", 2)}`);
-console.log(`Operator = Subtract 5 - 3: ${operate(5, "-", 3)}`);
-console.log(`Operator = divide 10 / 2: ${operate(10, "/", 2)}`);
-console.log(`Operator = Multiply 3 * 2: ${operate(3, "*", 2)}`);
-
 
